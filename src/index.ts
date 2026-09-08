5dc871961a5198714e5d92281a7e5603c1f703d7
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-08 18:25:09 +0530

    chore: refine public type exports

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { BooleanValidator } from "./validators/boolean";
 import { EnumValidator } from "./validators/enum";
 import { NumberValidator } from "./validators/number";
 import { StringValidator } from "./validators/string";
+import type { AnyValidator, InferEnv, InferValidator } from "./types";
 
 export { createEnv } from "./createEnv";
 export { SafeEnvError } from "./errors";
-export type { InferEnv, InferValidator } from "./types";
+export type { AnyValidator, InferEnv, InferValidator } from "./types";
 
 export const z = {
   string: () => new StringValidator(),
@@ -15,4 +16,8 @@ export const z = {
   enum: <const T extends readonly string[]>(values: T) => new EnumValidator(values),
 };
 
-export type EnvSchema = Parameters<typeof createEnv>[0];
+export type EnvSchema = Record<string, AnyValidator>;
+export type InferSchema<T extends EnvSchema> = InferEnv<T>;
+
+void createEnv;
+void (null as unknown as InferValidator<AnyValidator>);
