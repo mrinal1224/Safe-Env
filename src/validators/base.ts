e013542eefac5a73dbceb8bdd0c173cb892fdf13
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-08 18:27:12 +0530

    fix(core): preserve optional and default type semantics

--- src/validators/base.ts
+++ src/validators/base.ts
@@ -1,30 +1,33 @@
-export abstract class BaseValidator<T> {
+export type ValidatorOutput<T> = T;
+
+export abstract class BaseValidator<T, TOutput = T> {
   protected isOptional = false;
   protected hasDefault = false;
   protected defaultValue!: T;
 
-  optional(): this {
+  optional(): BaseValidator<T, T | undefined> {
     this.isOptional = true;
-    return this;
+    return this as unknown as BaseValidator<T, T | undefined>;
   }
 
-  default(value: T): this {
+  default(value: T): BaseValidator<T, T> {
     this.defaultValue = value;
     this.hasDefault = true;
-    return this;
+    this.isOptional = false;
+    return this as unknown as BaseValidator<T, T>;
   }
 
-  protected resolveUndefined(key: string): T | undefined {
+  protected resolveUndefined(key: string): TOutput {
     if (this.hasDefault) {
-      return this.defaultValue;
+      return this.defaultValue as TOutput;
     }
 
     if (this.isOptional) {
-      return undefined;
+      return undefined as TOutput;
     }
 
     throw new Error(`[safe-env] Missing required environment variable: ${key}`);
   }
 
-  abstract parse(value: string | undefined, key: string): T | undefined;
+  abstract parse(value: string | undefined, key: string): TOutput;
 }
